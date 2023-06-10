44557071c6c7a6d8168248b44399125bc709d8b2
xfdipzone <xfdipzone@gmail.com>
2023-06-10 23:42:25 +0800

    feat: develop php-http-request
    update request set and response class.

--- php-http-request/HttpRequest/RequestSet.php
+++ php-http-request/HttpRequest/RequestSet.php
@@ -10,5 +10,68 @@ namespace HttpRequest;
  */
 class RequestSet
 {
+    /**
+     * 保存 \HttpRequest\Type::FORM_DATA 类型的请求对象集合
+     *
+     * @var array [] \HttpRequest\FormData
+     */
+    private $form_data_set = [];
 
+    /**
+     * 保存 \HttpRequest\Type::FILE_DATA 类型的请求对象集合
+     *
+     * @var array [] \HttpRequest\FileData
+     */
+    private $file_data_set = [];
+
+    /**
+     * 添加请求对象
+     *
+     * @author fdipzone
+     * @DateTime 2023-06-10 23:15:10
+     *
+     * @param \HttpRequest\IRequestData $request_data
+     * @return void
+     */
+    public function add(\HttpRequest\IRequestData $request_data):void
+    {
+        if($request_data->type()==\HttpRequest\Type::FORM_DATA)
+        {
+            array_push($this->form_data_set, $request_data);
+        }
+        elseif($request_data->type()==\HttpRequest\Type::FILE_DATA)
+        {
+            array_push($this->file_data_set, $request_data);
+        }
+        else
+        {
+            throw new \Exception('request data type not supported');
+        }
+    }
+
+    /**
+     * 获取  \HttpRequest\Type::FORM_DATA 类型的请求对象集合
+     *
+     * @author fdipzone
+     * @DateTime 2023-06-10 23:39:47
+     *
+     * @return array
+     */
+    public function formDataSet():array
+    {
+        return $this->form_data_set;
+    }
+
+    /**
+     * 获取  \HttpRequest\Type::FILE_DATA 类型的请求对象集合
+     *
+     * @author fdipzone
+     * @DateTime 2023-06-10 23:40:11
+     *
+     * @return array
+     */
+    public function fileDataSet():array
+    {
+        return $this->file_data_set;
+    }
 }
--- php-http-request/HttpRequest/Response.php
+++ php-http-request/HttpRequest/Response.php
@@ -10,5 +10,115 @@ namespace HttpRequest;
  */
 class Response
 {
+    /**
+     * 执行状态
+     * true: 执行成功
+     * false: 执行失败
+     *
+     * @var boolean
+     */
+    private $success = true;
 
+    /**
+     * 错误信息，当success=false时非空
+     *
+     * @var string
+     */
+    private $err_msg = '';
+
+    /**
+     * 返回的数据
+     *
+     * @var string
+     */
+    private $data = '';
+
+    /**
+     * 设置执行状态
+     *
+     * @author fdipzone
+     * @DateTime 2023-06-10 22:49:42
+     *
+     * @param boolean $success 执行状态 true/false
+     * @return void
+     */
+    public function setSuccess(bool $success):void
+    {
+        $this->success = $success;
+    }
+
+    /**
+     * 获取执行状态
+     *
+     * @author fdipzone
+     * @DateTime 2023-06-10 22:50:05
+     *
+     * @return boolean
+     */
+    public function success():bool
+    {
+        return $this->success;
+    }
+
+    /**
+     * 设置错误信息
+     *
+     * @author fdipzone
+     * @DateTime 2023-06-10 22:50:14
+     *
+     * @param string $err_msg 错误信息
+     * @return void
+     */
+    public function setErrMsg(string $err_msg):void
+    {
+        if(empty($err_msg))
+        {
+            throw new \Exception('err msg is empty');
+        }
+        $this->err_msg = $err_msg;
+    }
+
+    /**
+     * 获取错误信息
+     *
+     * @author fdipzone
+     * @DateTime 2023-06-10 22:50:37
+     *
+     * @return string
+     */
+    public function errMsg():string
+    {
+        return $this->err_msg;
+    }
+
+    /**
+     * 设置返回数据
+     *
+     * @author fdipzone
+     * @DateTime 2023-06-10 22:50:47
+     *
+     * @param string $data 返回数据
+     * @return void
+     */
+    public function setData(string $data):void
+    {
+        if(empty($data))
+        {
+            throw new \Exception('response data is empty');
+        }
+        $this->data = $data;
+    }
+
+    /**
+     * 获取返回数据
+     *
+     * @author fdipzone
+     * @DateTime 2023-06-10 22:51:06
+     *
+     * @return string
+     */
+    public function data():string
+    {
+        return $this->data;
+    }
 }
