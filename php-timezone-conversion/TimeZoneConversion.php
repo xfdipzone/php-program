ac74500c2fa47e6024a0ab8ea0b29ed3a127bb9e
xfdipzone <xfdipzone@gmail.com>
2024-07-15 22:27:20 +0800

    docs: update php-timezone-conversion comments

--- php-timezone-conversion/TimeZoneConversion.php
+++ php-timezone-conversion/TimeZoneConversion.php
@@ -10,6 +10,7 @@ class TimeZoneConversion
 {
     /**
      * 日期时间时区转换
+     * 将原始日期时间转为指定时区的日期时间
      *
      * 时区格式使用 GMT+0000
      * 日期时间字符串格式参考：https://www.php.net/manual/zh/datetime.formats.php
@@ -21,7 +22,7 @@ class TimeZoneConversion
      * @param string $ori_datetime 原始日期时间
      * @param string $ori_timezone 原始时区
      * @param string $dest_timezone 目标时区
-     * @param string $dest_format 目标日期时间格式化格式
+     * @param string $dest_format 目标日期时间输出格式
      * @return array [datetime=>xxx, timezone=>xxx]
      */
     public static function convert(string $ori_datetime, string $ori_timezone, string $dest_timezone='GMT+0800', string $dest_format='Y-m-d H:i:s'):array
