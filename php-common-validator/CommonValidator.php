d4cec30e73658e538f4c5974d74e9e80cd664850
xfdipzone <xfdipzone@gmail.com>
2024-07-26 19:55:58 +0800

    feat: add php-common-validator validate functions
    add number,http,domain and Languages validate functions.

--- php-common-validator/CommonValidator.php
+++ php-common-validator/CommonValidator.php
@@ -53,4 +53,117 @@ class CommonValidator
         $length = mb_strlen($val, 'utf-8');
         return $length >= $min_length && $length <= $max_length? true : false;
     }
+
+    /**
+     * 判断数据是否数字
+     * 支持浮点数与负数判断
+     *
+     * @author fdipzone
+     * @DateTime 2024-07-26 13:15:50
+     *
+     * @param mixed $val 数据
+     * @param boolean $allow_float 是否允许浮点数 true: 允许 false: 不允许
+     * @param boolean $allow_signed 是否允许负数 true: 允许 false: 不允许
+     * @return boolean
+     */
+    public static function number($val, bool $allow_float=false, bool $allow_signed=false):bool
+    {
+        if($allow_signed)
+        {
+            return $allow_float? is_numeric($val) : preg_match('/^-?([0-9])+$/', $val);
+        }
+        else
+        {
+            return $allow_float? preg_match('/^([0-9])+([\.|,]([0-9])*)?$/', $val) : preg_match('/^([0-9])+$/', $val);
+        }
+    }
+
+    /**
+     * 判断是否http/https地址
+     *
+     * @author fdipzone
+     * @DateTime 2024-07-26 19:23:23
+     *
+     * @param string $val 字符串
+     * @return boolean
+     */
+    public static function http(string $val):bool
+    {
+        return preg_match('/^(https?:\/\/)?(([a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,6}))(?::\d+)?(?:\/[^\s?#]*)?(?:\?[^\s#]*)?(?:#[^\s]*)?$/is', $val);
+    }
+
+    /**
+     * 判断是否域名格式
+     *
+     * @author fdipzone
+     * @DateTime 2024-07-26 19:24:36
+     *
+     * @param string $val
+     * @return boolean
+     */
+    public static function domain(string $val):bool
+    {
+        $pattern = '/^([a-z0-9]+([a-z0-9-]*(?:[a-z0-9]+))?\.)?[a-z0-9]+([a-z0-9-]*(?:[a-z0-9]+))?(\.us|\.tv|\.org\.cn|\.org|\.net\.cn|\.net|\.mobi|\.me|\.la|\.info|\.hk|\.gov\.cn|\.edu|\.com\.cn|\.com|\.co\.jp|\.co|\.cn|\.cc|\.biz)$/i';
+        return strpos($val, '--') === false &&
+        preg_match($pattern, $val) ? true : false;
+    }
+
+    /**
+     * 判断字符串是否英文
+     *
+     * @author fdipzone
+     * @DateTime 2024-07-26 17:07:46
+     *
+     * @param string $val 字符串
+     * @return boolean
+     */
+    public static function isEnglish(string $val):bool
+    {
+        return preg_match('/^[A-Za-z]+$/', $val);
+    }
+
+    /**
+     * 判断字符串是否中文
+     *
+     * @author fdipzone
+     * @DateTime 2024-07-26 17:08:01
+     *
+     * @param string $val 字符串
+     * @return boolean
+     */
+    public static function isChinese(string $val):bool
+    {
+        return preg_match('/[\x{4e00}-\x{9fa5}]/u', $val);
+    }
+
+    /**
+     * 判断字符串是否泰文
+     *
+     * @author fdipzone
+     * @DateTime 2024-07-26 17:08:11
+     *
+     * @param string $val 字符串
+     * @return boolean
+     */
+    public static function isThai(string $val):bool
+    {
+        return preg_match('/^(?=.*?[\p{Thai}])[\p{Thai} ]+$/u', $val);
+    }
+
+    /**
+     * 判断字符串是否越南文
+     * 越南文字符集中包含英文字符，因此不能区分越南文与英文
+     * 传入字符串为英文时，会返回 true
+     *
+     *
+     * @author fdipzone
+     * @DateTime 2024-07-26 17:08:22
+     *
+     * @param string $val 字符串
+     * @return boolean
+     */
+    public static function isVietnamese(string $val):bool
+    {
+        return preg_match('/[\p{Script=Latin}]+/u', $val);
+    }
 }
--- php-common-validator/demo.php
+++ php-common-validator/demo.php
@@ -2,7 +2,10 @@
 require 'CommonValidator.php';
 
 // 测试的方法集合$
-$test_funcs = ['TestEmpty', 'TestLength'];
+$test_funcs = [
+    'TestEmpty', 'TestLength', 'TestNumber', 'TestIsEnglish', 'TestHttp', 'TestDomain',
+    'TestIsChinese', 'TestIsThai', 'TestIsVietnamese'
+];
 
 foreach($test_funcs as $func)
 {
@@ -59,6 +62,12 @@ function TestLength()
             'want_ret' => true
         ),
         array(
+            'val' => 'abc',
+            'min_length' => 3,
+            'max_length' => 3,
+            'want_ret' => true
+        ),
+        array(
             'val' => 'a',
             'min_length' => 2,
             'max_length' => 5,
@@ -84,5 +93,234 @@ function TestLength()
         {
             assert($case['want_exception']==$e->getMessage());
         }
+    }
+}
+
+// 测试 number
+function TestNumber()
+{
+    $cases = array(
+        array(
+            'val' => 123,
+            'allow_float' => false,
+            'allow_signed' => false,
+            'want_ret' => true
+        ),
+        array(
+            'val' => 123.45,
+            'allow_float' => false,
+            'allow_signed' => false,
+            'want_ret' => false
+        ),
+        array(
+            'val' => 123.45,
+            'allow_float' => true,
+            'allow_signed' => false,
+            'want_ret' => true
+        ),
+        array(
+            'val' => -123.45,
+            'allow_float' => true,
+            'allow_signed' => false,
+            'want_ret' => false
+        ),
+        array(
+            'val' => -123.45,
+            'allow_float' => true,
+            'allow_signed' => true,
+            'want_ret' => true
+        ),
+        array(
+            'val' => 'abc',
+            'allow_float' => false,
+            'allow_signed' => false,
+            'want_ret' => false
+        ),
+        array(
+            'val' => null,
+            'allow_float' => false,
+            'allow_signed' => false,
+            'want_ret' => false
+        ),
+    );
+
+    foreach($cases as $case)
+    {
+        $ret = \CommonValidator::number($case['val'], $case['allow_float'], $case['allow_signed']);
+        assert($case['want_ret']==$ret);
+    }
+}
+
+// 测试 http
+function TestHttp()
+{
+    $cases = array(
+        array(
+            'val' => 'http://www.fdipzone.com',
+            'want_ret' => true
+        ),
+        array(
+            'val' => 'https://www.fdipzone.com',
+            'want_ret' => true
+        ),
+        array(
+            'val' => 'ftp://www.fdipzone.com',
+            'want_ret' => false
+        ),
+        array(
+            'val' => 'http://fdipzone',
+            'want_ret' => false
+        ),
+        array(
+            'val' => 'https://www.fdipzone..com',
+            'want_ret' => false
+        ),
+    );
+
+    foreach($cases as $case)
+    {
+        $ret = \CommonValidator::http($case['val']);
+        assert($case['want_ret']==$ret);
+    }
+}
+
+// 测试 domain
+function TestDomain()
+{
+    $cases = array(
+        array(
+            'val' => 'www.fdipzone.com',
+            'want_ret' => true
+        ),
+        array(
+            'val' => 'www.fdipzone.net',
+            'want_ret' => true
+        ),
+        array(
+            'val' => 'www.fdipzone..com',
+            'want_ret' => false
+        ),
+    );
+
+    foreach($cases as $case)
+    {
+        $ret = \CommonValidator::domain($case['val']);
+        assert($case['want_ret']==$ret);
+    }
+}
+
+// 测试 isEnglish
+function TestIsEnglish()
+{
+    $cases = array(
+        array(
+            'val' => 'hello',
+            'want_ret' => true
+        ),
+        array(
+            'val' => '你好',
+            'want_ret' => false
+        ),
+        array(
+            'val' => 'สวัสดี',
+            'want_ret' => false
+        ),
+        array(
+            'val' => 'Xin chào',
+            'want_ret' => false
+        ),
+    );
+
+    foreach($cases as $case)
+    {
+        $ret = \CommonValidator::isEnglish($case['val']);
+        assert($case['want_ret']==$ret);
+    }
+}
+
+// 测试 isChinese
+function TestIsChinese()
+{
+    $cases = array(
+        array(
+            'val' => '你好',
+            'want_ret' => true
+        ),
+        array(
+            'val' => 'hello',
+            'want_ret' => false
+        ),
+        array(
+            'val' => 'สวัสดี',
+            'want_ret' => false
+        ),
+        array(
+            'val' => 'Xin chào',
+            'want_ret' => false
+        ),
+    );
+
+    foreach($cases as $case)
+    {
+        $ret = \CommonValidator::isChinese($case['val']);
+        assert($case['want_ret']==$ret);
+    }
+}
+
+// 测试 isThai
+function TestIsThai()
+{
+    $cases = array(
+        array(
+            'val' => 'สวัสดี',
+            'want_ret' => true
+        ),
+        array(
+            'val' => 'hello',
+            'want_ret' => false
+        ),
+        array(
+            'val' => '你好',
+            'want_ret' => false
+        ),
+        array(
+            'val' => 'Xin chào',
+            'want_ret' => false
+        ),
+    );
+
+    foreach($cases as $case)
+    {
+        $ret = \CommonValidator::isThai($case['val']);
+        assert($case['want_ret']==$ret);
+    }
+}
+
+// 测试 isVietnamese
+function TestIsVietnamese()
+{
+    $cases = array(
+        array(
+            'val' => 'Xin chào',
+            'want_ret' => true
+        ),
+        array(
+            'val' => 'hello',
+            'want_ret' => true
+        ),
+        array(
+            'val' => '你好',
+            'want_ret' => false
+        ),
+        array(
+            'val' => 'สวัสดี',
+            'want_ret' => false
+        ),
+    );
+
+    foreach($cases as $case)
+    {
+        $ret = \CommonValidator::isVietnamese($case['val']);
+        assert($case['want_ret']==$ret);
     }
 }
