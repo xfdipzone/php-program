9c8d417b26940bf0d7e35799ca7079abdce92268
xfdipzone <xfdipzone@gmail.com>
2024-08-05 13:45:18 +0800

    refactor: update php-validator IDNumberValidator
    1.update IDNumberValidator license plate number validate function.
    2.add IDNumberValidator license plate number validate demo.

--- php-validator/Validator/IDNumberValidator.php
+++ php-validator/Validator/IDNumberValidator.php
@@ -114,7 +114,7 @@ class IDNumberValidator
      */
     public static function licensePlateNumber(string $license_plate):bool
     {
-        $pattern = '/^(([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-Z](([0-9]{5}[DF])|([DF]([A-HJ-NP-Z0-9])[0-9]{4})))|([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-Z][A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9挂学警港澳使领]))$/';
+        $pattern = '/^(([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-Z](([0-9]{5}[DF])|([DF]([A-HJ-NP-Z0-9])[0-9]{4})))|([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-Z][A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9挂学警港澳使领]))$/u';
         return preg_match($pattern, $license_plate);
     }
 }
--- php-validator/idnumber_validator_demo.php
+++ php-validator/idnumber_validator_demo.php
@@ -3,7 +3,7 @@ require 'autoload.php';
 
 // 测试的方法集合
 $test_funcs = [
-    'TestIDCard',
+    'TestIDCard', 'TestLicensePlateNumber',
 ];
 
 foreach($test_funcs as $func)
@@ -37,6 +37,47 @@ function TestIDCard()
     foreach($cases as $case)
     {
         $ret = \Validator\IDNumberValidator::idCard($case['val']);
+        assert($case['want_ret']==$ret);
+    }
+}
+
+// 测试 licensePlateNumber
+function TestLicensePlateNumber()
+{
+    $cases = array(
+        array(
+            'val' => '京AD12345',
+            'want_ret' => true
+        ),
+        array(
+            'val' => '粤AF12345',
+            'want_ret' => true
+        ),
+        array(
+            'val' => '粤B12345D',
+            'want_ret' => true
+        ),
+        array(
+            'val' => '渝A1234挂',
+            'want_ret' => true
+        ),
+        array(
+            'val' => 'ABC1234',
+            'want_ret' => false
+        ),
+        array(
+            'val' => '京AE12345',
+            'want_ret' => false
+        ),
+        array(
+            'val' => '辽A12345D1',
+            'want_ret' => false
+        ),
+    );
+
+    foreach($cases as $case)
+    {
+        $ret = \Validator\IDNumberValidator::licensePlateNumber($case['val']);
         assert($case['want_ret']==$ret);
     }
 }
