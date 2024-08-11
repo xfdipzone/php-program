38d1fc8da2b140e4c90825d0085ef1223aea9e03
xfdipzone <xfdipzone@gmail.com>
2024-08-11 19:21:58 +0800

    refactor: update php-sensitive-word-filter MemoryStorage and FileStorage
    add save function to append sensitive words.

--- php-sensitive-word-filter/SensitiveWordFilter/Storage/FileStorage.php
+++ php-sensitive-word-filter/SensitiveWordFilter/Storage/FileStorage.php
@@ -3,7 +3,7 @@ namespace SensitiveWordFilter\Storage;
 
 /**
  * 基于文件存储敏感词
- * 文件一行存储一个敏感词
+ * 文件一行存储一个敏感词，使用换行符分隔
  *
  * @author fdipzone
  * @DateTime 2024-08-10 23:18:53
@@ -11,13 +11,6 @@ namespace SensitiveWordFilter\Storage;
  */
 class FileStorage implements \SensitiveWordFilter\Storage\IStorage
 {
-    /**
-     * 敏感词文件路径
-     *
-     * @var string
-     */
-    private $sensitive_word_file;
-
     /**
      * 敏感词集合
      * key=>value key:敏感词 value:true
@@ -27,22 +20,55 @@ class FileStorage implements \SensitiveWordFilter\Storage\IStorage
     private $sensitive_words = [];
 
     /**
-     * 初始化
-     * 设置敏感词文件路径
+     * 从敏感词文件中读取敏感词列表
+     * 此方法可多次调用，读取多个文件的敏感词，追加到敏感词列表
      *
      * @author fdipzone
-     * @DateTime 2024-08-10 23:21:41
+     * @DateTime 2024-08-11 18:43:23
      *
      * @param string $sensitive_word_file 敏感词文件路径
+     * @return void
      */
-    public function __construct(string $sensitive_word_file)
+    public function save(string $sensitive_word_file):void
     {
         if(!file_exists($sensitive_word_file))
         {
             throw new \Exception('file storage: sensitive word file not exists');
         }
 
-        $this->sensitive_word_file = $sensitive_word_file;
+        $new_sensitive_words = $this->parseSensitiveWordFile($sensitive_word_file);
+        $this->sensitive_words = array_merge($this->sensitive_words, $new_sensitive_words);
+    }
+
+    /**
+     * 解析敏感词文件数据为敏感词列表
+     *
+     * @author fdipzone
+     * @DateTime 2024-08-11 18:55:34
+     *
+     * @param string $sensitive_word_file 敏感词文件路径
+     * @return array
+     */
+    private function parseSensitiveWordFile(string $sensitive_word_file):array
+    {
+        $sensitive_words = [];
+
+        $file_data = file_get_contents($sensitive_word_file);
+        $file_sensitive_words = explode(PHP_EOL, $file_data);
+
+        if(count($file_sensitive_words)>0)
+        {
+            foreach($file_sensitive_words as $word)
+            {
+                $word = str_replace(array(' ', chr(10), chr(13)), '', $word);
+                if($word!='')
+                {
+                    $sensitive_words[] = $word;
+                }
+            }
+        }
+
+        return $sensitive_words;
     }
 
     /**
--- php-sensitive-word-filter/SensitiveWordFilter/Storage/MemoryStorage.php
+++ php-sensitive-word-filter/SensitiveWordFilter/Storage/MemoryStorage.php
@@ -19,22 +19,24 @@ class MemoryStorage implements \SensitiveWordFilter\Storage\IStorage
     private $sensitive_words = [];
 
     /**
-     * 初始化
      * 设置敏感词列表
+     * 此方法可多次调用，追加到敏感词列表
      *
      * @author fdipzone
-     * @DateTime 2024-08-08 18:16:33
+     * @DateTime 2024-08-11 18:42:20
      *
      * @param array $sensitive_words 敏感词列表
+     * @return void
      */
-    public function __construct(array $sensitive_words)
+    public function save(array $sensitive_words):void
     {
         if(!$sensitive_words)
         {
             throw new \Exception('memory storage: sensitive words is empty');
         }
 
-        $this->sensitive_words = array_fill_keys($sensitive_words, true);
+        $new_sensitive_words = array_fill_keys($sensitive_words, true);
+        $this->sensitive_words = array_merge($this->sensitive_words, $new_sensitive_words);
     }
 
     /**
