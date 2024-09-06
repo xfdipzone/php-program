6b72d7baef22c2a01403f5e6bcc4aa85c063a460
xfdipzone <xfdipzone@gmail.com>
2024-09-06 23:26:21 +0800

    style: update php-hexfile code indent

--- php-hexfile/HexFileConverter.php
+++ php-hexfile/HexFileConverter.php
@@ -6,8 +6,8 @@
  * @DateTime 2023-03-21 20:03:12
  *
  */
-class HexFileConverter{
-
+class HexFileConverter
+{
     /**
      * 文件转为16进制串
      *
@@ -17,8 +17,10 @@ class HexFileConverter{
      * @param string $file 文件
      * @return string
      */
-    public static function fileToHex(string $file):string{
-        if(!file_exists($file)){
+    public static function fileToHex(string $file):string
+    {
+        if(!file_exists($file))
+        {
             throw new \Exception('file not exists');
         }
 
@@ -36,13 +38,18 @@ class HexFileConverter{
      * @param string $file    生成的文件
      * @return boolean
      */
-    public static function hexToFile(string $hex_str, string $file):bool{
-        try{
+    public static function hexToFile(string $hex_str, string $file):bool
+    {
+        try
+        {
             $data = pack('H*', $hex_str);
             file_put_contents($file, $data, true);
-        }catch(\Throwable $e){
+        }
+        catch(\Throwable $e)
+        {
             throw new \Exception('hex str convert file fail');
         }
+
         return true;
     }
 
