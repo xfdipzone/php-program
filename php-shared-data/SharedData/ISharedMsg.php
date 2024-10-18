49896e464484a9e85b1901be483a3fd06fcb58ff
xfdipzone <xfdipzone@gmail.com>
2024-10-18 21:43:11 +0800

    feat: init php-shared-data shared msg
    add shared msg interface.

---
+++ php-shared-data/SharedData/ISharedMsg.php
@@ -0,0 +1,34 @@
+<?php
+namespace SharedData;
+
+/**
+ * 共享消息队列接口
+ * 定义共享消息队列类需要实现的方法
+ *
+ * @author fdipzone
+ * @DateTime 2024-10-18 21:33:39
+ *
+ */
+interface ISharedMsg
+{
+    /**
+     * 发送消息
+     *
+     * @author fdipzone
+     * @DateTime 2024-10-18 21:38:20
+     *
+     * @param string $msg 消息内容
+     * @return boolean
+     */
+    public function send(string $msg):bool;
+
+    /**
+     * 接收消息
+     *
+     * @author fdipzone
+     * @DateTime 2024-10-18 21:38:32
+     *
+     * @return string
+     */
+    public function receive():string;
+}
