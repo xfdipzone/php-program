fbf18b619fa844ce0a2513c3ee2083ef0df2c9c3
xfdipzone <xfdipzone@gmail.com>
2024-10-30 22:27:43 +0800

    feat: add php-mailer mailer setting and getting method
    1.add Sender, Recipient, Attachment setting and getting method.
    2.supplementary mailer unit test.

--- php-mailer/Mail/Mailer.php
+++ php-mailer/Mail/Mailer.php
@@ -20,6 +20,41 @@ class Mailer
     private $server_config;
 
     /**
+     * 发件人
+     *
+     * @var \Mail\Sender
+     */
+    private $sender;
+
+    /**
+     * 收件人集合
+     *
+     * @var array [] \Mail\Recipient
+     */
+    private $recipients;
+
+    /**
+     * 抄送收件人集合
+     *
+     * @var array [] \Mail\Recipient
+     */
+    private $ccRecipients;
+
+    /**
+     * 密件抄送收件人集合
+     *
+     * @var array [] \Mail\Recipient
+     */
+    private $bccRecipients;
+
+    /**
+     * 附件集合
+     *
+     * @var array [] \Mail\Attachment
+     */
+    private $attachments;
+
+    /**
      * 初始化
      * 设置电子邮件服务器配置
      *
@@ -31,5 +66,140 @@ class Mailer
     public function __construct(\Mail\ServerConfig $server_config)
     {
         $this->server_config = $server_config;
+    }
+
+    /**
+     * 设置发件人
+     *
+     * @author fdipzone
+     * @DateTime 2024-10-30 18:40:47
+     *
+     * @param \Mail\Sender $sender 发件人对象
+     * @return void
+     */
+    public function setSender(\Mail\Sender $sender):void
+    {
+        $this->sender = $sender;
+    }
+
+    /**
+     * 获取发件人
+     *
+     * @author fdipzone
+     * @DateTime 2024-10-30 19:21:04
+     *
+     * @return \Mail\Sender
+     */
+    public function sender():\Mail\Sender
+    {
+        return $this->sender;
+    }
+
+    /**
+     * 添加收件人
+     *
+     * @author fdipzone
+     * @DateTime 2024-10-30 18:51:20
+     *
+     * @param \Mail\Recipient $recipient 收件人
+     * @return void
+     */
+    public function addRecipient(\Mail\Recipient $recipient):void
+    {
+        $this->recipients[$recipient->email()] = $recipient;
+    }
+
+    /**
+     * 获取收件人集合
+     *
+     * @author fdipzone
+     * @DateTime 2024-10-30 19:22:07
+     *
+     * @return array [] \Mail\Recipient
+     */
+    public function recipients():array
+    {
+        return $this->recipients;
+    }
+
+    /**
+     * 添加抄送收件人
+     *
+     * @author fdipzone
+     * @DateTime 2024-10-30 19:03:18
+     *
+     * @param \Mail\Recipient $recipient 抄送收件人
+     * @return void
+     */
+    public function addCcRecipient(\Mail\Recipient $recipient):void
+    {
+        $this->ccRecipients[$recipient->email()] = $recipient;
+    }
+
+    /**
+     * 获取抄送收件人集合
+     *
+     * @author fdipzone
+     * @DateTime 2024-10-30 19:23:00
+     *
+     * @return array [] \Mail\Recipient
+     */
+    public function ccRecipients():array
+    {
+        return $this->ccRecipients;
+    }
+
+    /**
+     * 添加密件抄送收件人
+     *
+     * @author fdipzone
+     * @DateTime 2024-10-30 19:03:18
+     *
+     * @param \Mail\Recipient $recipient 密件抄送收件人
+     * @return void
+     */
+    public function addBccRecipient(\Mail\Recipient $recipient):void
+    {
+        $this->bccRecipients[$recipient->email()] = $recipient;
+    }
+
+    /**
+     * 获取密件抄送收件人集合
+     *
+     * @author fdipzone
+     * @DateTime 2024-10-30 19:23:39
+     *
+     * @return array [] \Mail\Recipient
+     */
+    public function bccRecipients():array
+    {
+        return $this->bccRecipients;
+    }
+
+    /**
+     * 添加附件
+     *
+     * @author fdipzone
+     * @DateTime 2024-10-30 18:53:09
+     *
+     * @param \Mail\Attachment $attachment 附件
+     * @return void
+     */
+    public function addAttachment(\Mail\Attachment $attachment):void
+    {
+        $this->attachments[$attachment->file()] = $attachment;
+    }
+
+    /**
+     * 获取附件集合
+     *
+     * @author fdipzone
+     * @DateTime 2024-10-30 19:25:05
+     *
+     * @return array [] \Mail\attachment
+     */
+    public function attachments():array
+    {
+        return $this->attachments;
     }
 }
--- tests/Mail/MailerTest.php
+++ tests/Mail/MailerTest.php
@@ -24,4 +24,126 @@ final class MailerTest extends TestCase
         $mailer = new \Mail\Mailer($server_config);
         $this->assertEquals('Mail\Mailer', get_class($mailer));
     }
+
+    /**
+     * @covers \Mail\Mailer::setSender
+     * @covers \Mail\Mailer::sender
+     */
+    public function testSender()
+    {
+        $smtp_host = 'smtp.mxhichina.com';
+        $smtp_username = 'fdipzone';
+        $smtp_password = '123456abc!@#$';
+        $server_config = new \Mail\ServerConfig($smtp_host, $smtp_username, $smtp_password);
+        $mailer = new \Mail\Mailer($server_config);
+
+        $from_email = 'technology@zone.com';
+        $from_name = 'fdipzone';
+        $sender = new \Mail\Sender($from_email, $from_name);
+
+        $mailer->setSender($sender);
+        $mailer_sender = $mailer->sender();
+        $this->assertEquals($from_email, $mailer_sender->fromEmail());
+        $this->assertEquals($from_name, $mailer_sender->fromName());
+        $this->assertEquals('', $mailer_sender->senderEmail());
+    }
+
+    /**
+     * @covers \Mail\Mailer::addRecipient
+     * @covers \Mail\Mailer::recipients
+     */
+    public function testRecipient()
+    {
+        $smtp_host = 'smtp.mxhichina.com';
+        $smtp_username = 'fdipzone';
+        $smtp_password = '123456abc!@#$';
+        $server_config = new \Mail\ServerConfig($smtp_host, $smtp_username, $smtp_password);
+        $mailer = new \Mail\Mailer($server_config);
+
+        $email = 'technology@zone.com';
+        $name = 'fdipzone';
+        $recipient = new \Mail\Recipient($email, $name);
+
+        $mailer->addRecipient($recipient);
+        $mailer_recipients = $mailer->recipients();
+        $this->assertSame(1, count($mailer_recipients));
+        $this->assertEquals($email, $mailer_recipients[$email]->email());
+        $this->assertEquals($name, $mailer_recipients[$email]->name());
+    }
+
+    /**
+     * @covers \Mail\Mailer::addCcRecipient
+     * @covers \Mail\Mailer::ccRecipients
+     */
+    public function testCcRecipient()
+    {
+        $smtp_host = 'smtp.mxhichina.com';
+        $smtp_username = 'fdipzone';
+        $smtp_password = '123456abc!@#$';
+        $server_config = new \Mail\ServerConfig($smtp_host, $smtp_username, $smtp_password);
+        $mailer = new \Mail\Mailer($server_config);
+
+        $email = 'technology@zone.com';
+        $name = 'fdipzone';
+        $recipient = new \Mail\Recipient($email, $name);
+
+        $mailer->addCcRecipient($recipient);
+        $mailer_cc_recipients = $mailer->ccRecipients();
+        $this->assertSame(1, count($mailer_cc_recipients));
+        $this->assertEquals($email, $mailer_cc_recipients[$email]->email());
+        $this->assertEquals($name, $mailer_cc_recipients[$email]->name());
+    }
+
+    /**
+     * @covers \Mail\Mailer::addBccRecipient
+     * @covers \Mail\Mailer::bccRecipients
+     */
+    public function testBccRecipient()
+    {
+        $smtp_host = 'smtp.mxhichina.com';
+        $smtp_username = 'fdipzone';
+        $smtp_password = '123456abc!@#$';
+        $server_config = new \Mail\ServerConfig($smtp_host, $smtp_username, $smtp_password);
+        $mailer = new \Mail\Mailer($server_config);
+
+        $email = 'technology@zone.com';
+        $name = 'fdipzone';
+        $recipient = new \Mail\Recipient($email, $name);
+
+        $mailer->addBccRecipient($recipient);
+        $mailer_bcc_recipients = $mailer->bccRecipients();
+        $this->assertSame(1, count($mailer_bcc_recipients));
+        $this->assertEquals($email, $mailer_bcc_recipients[$email]->email());
+        $this->assertEquals($name, $mailer_bcc_recipients[$email]->name());
+    }
+
+    /**
+     * @covers \Mail\Mailer::addAttachment
+     * @covers \Mail\Mailer::attachments
+     */
+    public function testAttachment()
+    {
+        $smtp_host = 'smtp.mxhichina.com';
+        $smtp_username = 'fdipzone';
+        $smtp_password = '123456abc!@#$';
+        $server_config = new \Mail\ServerConfig($smtp_host, $smtp_username, $smtp_password);
+        $mailer = new \Mail\Mailer($server_config);
+
+        $attachment_file = '/tmp/attachment.txt';
+        file_put_contents($attachment_file, 'attachment file');
+
+        $name = '附件.txt';
+        $attachment = new \Mail\Attachment($attachment_file, $name);
+
+        $mailer->addAttachment($attachment);
+        $mailer_attachments = $mailer->attachments();
+        $this->assertSame(1, count($mailer_attachments));
+        $this->assertEquals($attachment_file, $mailer_attachments[$attachment_file]->file());
+        $this->assertEquals($name, $mailer_attachments[$attachment_file]->name());
+
+        if(file_exists($attachment_file))
+        {
+            unlink($attachment_file);
+        }
+    }
 }
