8128f41952adf407f588e0d56461f5dc49bcab2b
xfdipzone <xfdipzone@gmail.com>
2026-04-08 16:18:56 +0800

    style: update php-cartesian-product formatting code

--- php-cartesian-product/CartesianProduct.php
+++ php-cartesian-product/CartesianProduct.php
@@ -6,8 +6,8 @@
  * @DateTime 2023-03-20 19:36:40
  *
  */
-class CartesianProduct{
-
+class CartesianProduct
+{
     /**
      * 计算多个集合的笛卡尔积
      *
@@ -17,16 +17,17 @@ class CartesianProduct{
      * @param array $sets 需要参与计算笛卡尔积的集合
      * @return array
      */
-    public static function cal(array $sets):array{
-
+    public static function cal(array $sets):array
+    {
         // 保存结果
         $result = array();
 
         // 循环遍历集合数据
-        for($i=0,$count=count($sets); $i<$count; $i++){
-
+        for($i=0,$count=count($sets); $i<$count; $i++)
+        {
             // 初始化，第一个集合，不用做笛卡尔积
-            if($i==0){
+            if($i==0)
+            {
                 $result = $sets[$i];
                 continue;
             }
@@ -35,19 +36,18 @@ class CartesianProduct{
             $tmp = array();
 
             // 结果与当前集合计算笛卡尔积
-            foreach($result as $res){
-                foreach($sets[$i] as $set){
+            foreach($result as $res)
+            {
+                foreach($sets[$i] as $set)
+                {
                     $tmp[] = $res.$set;
                 }
             }
 
             // 将笛卡尔积写入结果
             $result = $tmp;
-
         }
 
         return $result;
-
     }
-
 }
