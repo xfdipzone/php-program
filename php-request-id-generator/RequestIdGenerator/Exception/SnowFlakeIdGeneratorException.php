2ef9eb7f21c22b8d028cface7171af3b9be4b1ed
xfdipzone <xfdipzone@gmail.com>
2026-05-19 21:29:17 +0800

    docs: update php-request-id-generator
    update SnowFlakeIdGeneratorException class comment.

--- php-request-id-generator/RequestIdGenerator/Exception/SnowFlakeIdGeneratorException.php
+++ php-request-id-generator/RequestIdGenerator/Exception/SnowFlakeIdGeneratorException.php
@@ -5,7 +5,7 @@ namespace RequestIdGenerator\Exception;
  * 雪花算法 ID 生成器类异常
  *
  * @author fdipzone
- * @DateTime 2026-05-08 20:29:10
+ * @DateTime 2026-05-19 21:27:02
  *
  */
 class SnowFlakeIdGeneratorException extends \Exception
