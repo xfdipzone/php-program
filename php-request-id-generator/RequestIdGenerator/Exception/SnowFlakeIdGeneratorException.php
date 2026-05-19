8c277792ce5105bc6589cce9d390c3509bd425d7
xfdipzone <xfdipzone@gmail.com>
2026-05-19 21:22:20 +0800

    feat: add php-request-id-generator SnowFlakeIdGeneratorException class

---
+++ php-request-id-generator/RequestIdGenerator/Exception/SnowFlakeIdGeneratorException.php
@@ -0,0 +1,14 @@
+<?php
+namespace RequestIdGenerator\Exception;
+
+/**
+ * 雪花算法 ID 生成器类异常
+ *
+ * @author fdipzone
+ * @DateTime 2026-05-08 20:29:10
+ *
+ */
+class SnowFlakeIdGeneratorException extends \Exception
+{
+
+}
